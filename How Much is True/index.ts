0e4d122bbe060b7506d66ad64e2dda519fba6611
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-12-02 01:46:56 +0500

    count true in array

---
+++ How Much is True/index.ts
@@ -0,0 +1,24 @@
+/*
+How Much is True?
+Create a function which returns the number of true values there are in an array.
+
+Examples
+countTrue([true, false, false, true, false]) ➞ 2
+
+countTrue([false, false, false, false]) ➞ 0
+
+countTrue([]) ➞ 0
+Notes
+Return 0 if given an empty array.
+All array items are of the type bool (true or false).
+*/
+
+function countTrue(boolArray:boolean[]):number{
+    let countTrue=0
+    boolArray.map((i)=>{
+        if(i===true) countTrue++
+    })
+    return countTrue
+}
+
+console.log(countTrue([true, false, false, true,true,true,true, false]));
