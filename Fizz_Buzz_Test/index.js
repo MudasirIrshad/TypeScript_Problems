1982d219127231aafdeb699950d00a200f453217
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-12-04 18:30:54 +0500

    Fizz Buzz Test

---
+++ Fizz_Buzz_Test/index.js
@@ -0,0 +1,21 @@
+"use strict";
+function fizzBuzz(limit) {
+    let arr = [];
+    for (let i = 1; i <= limit; i++) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            arr.push('FizzBuzz');
+            continue;
+        }
+        else if (i % 5 == 0) {
+            arr.push("Buzz");
+            continue;
+        }
+        else if (i % 3 == 0) {
+            arr.push('Fizz');
+            continue;
+        }
+        arr.push(i);
+    }
+    return arr;
+}
+console.log(fizzBuzz(16));
